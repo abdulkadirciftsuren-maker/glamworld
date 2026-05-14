80cbe2a8d10abe269dc91307a227c497b5e6fc42
abdulkadirciftsuren-maker <abdulkadirciftsuren@gmail.com>
2026-05-14 23:46:30 +0200

    B24 - Zoom engelld (user-scalable=no) + Android edge-to-edge (viewport-fit+standalone) + Serit sadece anasayfada

--- src/components/UstSerit.jsx
+++ src/components/UstSerit.jsx
@@ -1,4 +1,8 @@
+import { useLocation } from 'react-router-dom';
 import './UstSerit.css';
+
 export default function UstSerit() {
+  const { pathname } = useLocation();
+  if (pathname !== '/') return null;
   return <div className="ust-serit" />;
 }
